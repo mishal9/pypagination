5df8ca798aa88428bf6fe2ae071b79e4b485a3f3
Mishal Shah <mshah31@asu.edu>
2021-02-25 00:31:34 -0700

    Updated

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pypagination",
+    version="0.0.1",
+    author="Mishal Shah",
+    author_email="mshah31@asu.edu",
+    description="A small library to paginate through json objects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mishal9/PyPaginate/issues",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
