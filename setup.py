03fc34ae44d0851698b75d53d2dac4ec973edec0
Mishal Shah <mshah31@asu.edu>
2021-02-25 00:45:58 -0700

    Updated README

--- setup.py
+++ setup.py
@@ -14,9 +14,10 @@ setuptools.setup(
     url="https://github.com/mishal9/pypagination/issues",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
 )
